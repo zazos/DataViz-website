76c95f442f1cbf6900e18bf5aaa641e9461deda9
AthinaVek <athina.vekraki@gmail.com>
2024-07-09 04:21:22 +0300

    attacks map upload

--- src/index.js
+++ src/index.js
@@ -18,9 +18,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import 'leaflet/dist/leaflet.css';
 
 import AdminLayout from "layouts/Admin/Admin.js";
-import RTLLayout from "layouts/RTL/RTL.js";
+// import RTLLayout from "layouts/RTL/RTL.js";
 
 import "assets/scss/black-dashboard-react.scss";
 import "assets/demo/demo.css";
@@ -38,7 +39,7 @@ root.render(
       <BrowserRouter>
         <Routes>
           <Route path="/admin/*" element={<AdminLayout />} />
-          <Route path="/rtl/*" element={<RTLLayout />} />
+          {/* <Route path="/rtl/*" element={<RTLLayout />} /> */}
           <Route
             path="*"
             element={<Navigate to="/admin/home" replace />}
