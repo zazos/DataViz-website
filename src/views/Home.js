559ebc080509df90ecbf0ab3dcc77701e4c34318
Panagiotis <pzazos@hotmail.com>
2024-07-10 12:54:54 +0300

    youtube link video updated

--- src/views/Home.js
+++ src/views/Home.js
@@ -40,7 +40,7 @@ function Home() {
               <iframe
                 width="100%"
                 height="320"
-                src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                src="https://www.youtube.com/embed/vfnIxu-WC4U"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
