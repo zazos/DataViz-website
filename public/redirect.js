ae2d8ca22ff6694f0f092451c9714c87915822a5
Panagiotis Zazos <pzazos@hotmail.com>
2024-07-10 18:54:26 +0300

    refresh redirects to home page

--- public/redirect.js
+++ public/redirect.js
@@ -1,9 +1,8 @@
 // public/redirect.js
 (function () {
-    const redirectTo = sessionStorage.redirectTo;
-    delete sessionStorage.redirectTo;
-    if (redirectTo && redirectTo !== location.href) {
-      history.replaceState(null, null, redirectTo);
-    }
-  })();
-  
+  const redirectTo = sessionStorage.redirectTo;
+  delete sessionStorage.redirectTo;
+  if (redirectTo && redirectTo !== location.href) {
+    history.replaceState(null, null, redirectTo);
+  }
+})();
