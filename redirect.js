5f6667587807ccee36fb9524b68c3cea4b9e8a4d
Panagiotis Zazos <pzazos@hotmail.com>
2024-07-10 18:39:02 +0300

    Updates

---
+++ redirect.js
@@ -0,0 +1,9 @@
+// public/redirect.js
+(function () {
+    const redirectTo = sessionStorage.redirectTo;
+    delete sessionStorage.redirectTo;
+    if (redirectTo && redirectTo !== location.href) {
+      history.replaceState(null, null, redirectTo);
+    }
+  })();
+  
