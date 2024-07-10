c0168e71e906e51962c855b6055ac596d6f056f0
Panagiotis Zazos <pzazos@hotmail.com>
2024-07-10 20:02:46 +0300

    Updates

--- redirect.js
+++ redirect.js
@@ -1,8 +1,13 @@
-// public/redirect.js
 (function () {
   const redirectTo = sessionStorage.redirectTo;
   delete sessionStorage.redirectTo;
   if (redirectTo && redirectTo !== location.href) {
     history.replaceState(null, null, redirectTo);
+  }
+
+  // Handle hash-based routing
+  if (location.hash && location.hash.startsWith("#")) {
+    const path = location.hash.substring(1); // Get the path without the hash symbol
+    history.replaceState(null, null, path);
   }
 })();
